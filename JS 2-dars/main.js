90417b01a4e4d8e310d327a8c4ff05cca77d5c52
Islom Turgunov <147710629+ToshmirzaevNomonjon@users.noreply.github.com>
2023-10-16 18:36:48 +0500

    JS 2-dars
    27-guruh

---
+++ JS 2-dars/main.js
@@ -0,0 +1,88 @@
+/*
+ if (agar)
+ else if (ypki bo'lmasas)
+ else (u holda)
+*/
+
+/*
+if ("salom" == "salom"){
+    console.log("Ular teng!")
+}
+else {
+    console.log("Ular teng emas!")
+}
+*/
+/*
+if ("It dars 18:00 da boshlanadi"){
+    console.log("Xa 18:00 da boshlandi");
+}
+
+else if ("It dars 18:00 da boshlanadi"){
+    console.log("Xa 18:00 da boshlandi");
+}
+else{
+    console.log("Xa shu vaqtda boshlanadi");
+}
+
+if (45 == 50){
+    console.log("Xa bular teng");
+}
+
+else if (45 == 50){
+    console.log("Xa bular teng");
+}
+else{
+    console.log("Jovoblar to'g'ri");
+}
+
+if (667 == 777){
+    console.log("Bu javoblarning btasi ham to'g'ri emas");
+}
+else{
+    console.log("Bu javoblarning btasi ham to'g'ri emas");
+}
+
+if ("salom = salom"){
+    console.log("Jovoblar teng!")
+}
+else{
+    console.log("Jovoblar notog'ri!")
+}
+*/
+/*
+let a = 10;
+let b = 46;
+let c = 49;
+let d = 55;
+*/
+
+/*if (a == 10  && b == 46 && c == 49 && d == 55){
+    console.log("Solishtirilgan qiymatlarning hammasi to'g'ri")
+}
+else{
+    console.log("Solishtirilgan qiymatlardan kamida bittasi no'tog'ri to'g'ri");
+}*/
+
+/*
+if (a == 8 || b == 44 || c ==47 || d==55){
+    console.log("Solishtirilgan qiymatlardan kamida bittasi tog'ri ")
+}
+else{
+    console.log("Solishtirilgan qiymatlarning hammasi no'to'g'ri")
+}
+*/
+
+/* Dat types in JS: undefined,boolean */
+
+/*let matn = "JavaScript"*/
+/*let son = 45;*/
+/*let text = "soz"*/
+/*document.write(son)*/
+/*let rost = true;*/
+
+let rost = 45 ==45 ? true:false;
+
+document.write(rost)
+
+
+
